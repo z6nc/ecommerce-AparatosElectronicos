38bedcd569168e76ce892fbb1ce3c5d20652e9fa
z6nc <andresvelasquezarotinco@gmail.com>
2023-11-26 16:34:19 -0500

    ccc

--- paginas/inicio.php
+++ paginas/inicio.php
@@ -8,7 +8,7 @@ include ("../config/conexion.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Document</title>
+    <title>ELECTROTECHZONE</title>
 </head>
 <body>
     <?php 
